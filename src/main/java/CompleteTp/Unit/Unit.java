e9188cfc8bf69d72cc208d3b287a40a1ab8e03e9
Agustin <agustin.titotto@ing.austral.edu.ar>
2023-05-15 11:00:33 -0300

    Expressions for Meter class

---
+++ src/main/java/CompleteTp/Unit/Unit.java
@@ -0,0 +1,20 @@
+package CompleteTp.Unit;
+
+public class Unit {
+
+    private String symbol;
+    private Integer power;
+
+    public Unit(String symbol, Integer power) {
+        this.symbol = symbol;
+        this.power = power;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+}
